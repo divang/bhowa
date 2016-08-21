c051387a0f92498daf24b323105f13c00ab9f71b
divang.sharma <divang.sharma@onmobile.com>
2016-08-22 00:23:11 +0530

    Change table names and columns

--- app/src/main/java/bhowa/dao/mysql/impl/Queries.java
+++ app/src/main/java/bhowa/dao/mysql/impl/Queries.java
@@ -4,16 +4,16 @@ public class Queries {
 
 	  //Queries
     public static final String loginQuery = 
-    		"SELECT userid FROM User_Login WHERE userid = ? and password = ?";
-    
+    		"SELECT user_id FROM User WHERE user_id = ? and password = ?";
+
     public static final String activityLoggingQuery = 
     		"Insert into User_Activity_Logging (UserName, Mobile, Activity, Comment, Time) values (? ,? ,?, ?, ?)";
   
     public static final String statementProcessedQuery =
-    		"Insert into Processed_Bank_Statement(Bank_Statement_FileName, Uploaded_Date) values (?, ?)";
+    		"Insert into Bank_Statement(Bank_Statement_FileName, Uploaded_Date) values (?, ?)";
     
     public static final String isStatementProcessedQuery =
-    		"Select Bank_Statement_FileName, Uploaded_Date FROM Processed_Bank_Statement WHERE Bank_Statement_FileName = ?";
+    		"Select Statement_FileName, Uploaded_Date FROM Bank_Statement WHERE Statement_FileName = ?";
     
     public static final String transactionQuery =
     	    "Insert into Transactions(StatementID, Name, Amount, "
