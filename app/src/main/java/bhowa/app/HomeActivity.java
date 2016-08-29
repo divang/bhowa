14da4891d32d3dfc9e455f4736787bba24849f2a
divang.sharma <divang.sharma@onmobile.com>
2016-08-29 20:12:32 +0530

    Add PDF parsing progressing msg

--- app/src/main/java/bhowa/app/HomeActivity.java
+++ app/src/main/java/bhowa/app/HomeActivity.java
@@ -1,5 +1,6 @@
 package bhowa.app;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,6 +16,8 @@ import bhowa.dao.mysql.impl.BankStatement;
 import bhowa.parser.BhowaParserFactory;
 
 public class HomeActivity extends DashBoardActivity {
+
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,15 +55,29 @@ public class HomeActivity extends DashBoardActivity {
     {
         new FileChooser(this).setFileListener(new FileChooser.FileSelectedListener() {
             @Override public void fileSelected(final File file) {
-                String path = file.getPath();
-                BankStatement bState = (BankStatement) BhowaParserFactory.getDBInstance().getAllTransaction(path);
+                final String path = file.getPath();
+
+                progress = ProgressDialog.show(HomeActivity.this, null, "Parsing PDF ...", true, false);
+                //progress.hide();
+                progress.show();
+                Thread taskThread = new Thread(new Runnable() {
+                    public void run() {
+
+                        BankStatement bState = (BankStatement) BhowaParserFactory.getDBInstance().getAllTransaction(path);
+                        Intent transactionReportIntent = new Intent(getApplicationContext(), TransactionReportActivity.class);
+                        transactionReportIntent.putExtra("report",bState);
+                        startActivityForResult(transactionReportIntent, 0);
+                        progress.dismiss();
+                        progress.cancel();
+
+                    }
+                });
+                taskThread.start();
+
                 //Insert raw data in DB. Table : 'Row_Data'
-                BhowaDatabaseFactory.getDBInstance().insertRawData(bState.rowdata);
+                //BhowaDatabaseFactory.getDBInstance().insertRawData(bState.rowdata);
 
                 //Display Parsed Transactions
-                Intent transactionReportIntent = new Intent(getApplicationContext(), TransactionReportActivity.class);
-                transactionReportIntent.putExtra("report",bState);
-                startActivityForResult(transactionReportIntent, 0);
 
             }}).showDialog();
     }
